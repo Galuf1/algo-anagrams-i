5a3f1ff965d14b4f55f8d40a7eb75606bea7525f
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-26 22:06:21 +0200

    solved

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,25 @@
 # Don't forget to run your tests!
 
+from operator import is_
+import string
+
+
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	list1 = list(string1.lower())
+	list2 = list(string2.lower())
+	for i,j in enumerate(list1):
+		try:
+			index = list2.index(j)
+		except: return False
+		if j == list2[index]:
+			list2.remove(list2[index])
+			continue
+		else:
+			return False
+	return True
+
+	
+
+
+
+is_character_match('charm', 'march')
