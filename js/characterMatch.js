8bd45244b2f3cf917aefa23e9c0aa3ec3c14103c
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 15:16:28 +0200

    done js

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,16 @@
 exports.isCharacterMatch = function(string1, string2) {
+    const list1 = string1.toLowerCase().split("")
+    let list2 = string2.toLowerCase().split("")
+
+    for (let i = 0; i < list1.length -1; i++) {
+        // const search = list2.find(x => x === list1[i]);
+        let splice = list2.indexOf(list1[i])
+        if (splice >= -1){
+            list2 = list2.splice(splice,splice)
+        } else {
+            return false;
+        }
+    }
+    return true
 
 };
